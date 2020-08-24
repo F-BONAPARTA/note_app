dbe95d3e045dbf717ffd9ff3aab7d1190c5e8ff7
Anas_Mohamed <anas_mohamed@ieee.org>
2020-08-24 04:30:17 +0200

    commit

---
+++ lib/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+var color0 = Color(0xFFFFAB91);
+var color1 = Color(0xFFFFCC80);
+var color2 = Color(0xFFE6EE9B);
+var color3 = Color(0xFF80DEEA);
+var color5 = Color(0xFF80CBC4);
+var color4 = Color(0xFFF48FB1);
